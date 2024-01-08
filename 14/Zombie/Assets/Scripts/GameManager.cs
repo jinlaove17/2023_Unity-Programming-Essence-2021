0a7ee21dedbc4d930fd9700fc47588eda7ef2c4f
JongWoo <jinlaove17@naver.com>
2024-01-08 14:52:24 +0900

    [Add] 14장 ~ 17장 실습 완료

--- 14/Zombie/Assets/Scripts/GameManager.cs
+++ 14/Zombie/Assets/Scripts/GameManager.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 
 // 점수와 게임 오버 여부를 관리하는 게임 매니저
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    private static GameManager m_instance; // 싱글톤이 할당될 static 변수
+
+    private int score = 0; // 현재 게임 점수
+    public bool isGameover { get; private set; } // 게임 오버 상태
+
     // 싱글톤 접근용 프로퍼티
     public static GameManager instance
     {
@@ -19,12 +25,8 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    private static GameManager m_instance; // 싱글톤이 할당될 static 변수
-
-    private int score = 0; // 현재 게임 점수
-    public bool isGameover { get; private set; } // 게임 오버 상태
-
-    private void Awake() {
+    private void Awake()
+    {
         // 씬에 싱글톤 오브젝트가 된 다른 GameManager 오브젝트가 있다면
         if (instance != this)
         {
@@ -33,27 +35,32 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    private void Start() {
+    private void Start()
+    {
         // 플레이어 캐릭터의 사망 이벤트 발생시 게임 오버
         FindObjectOfType<PlayerHealth>().onDeath += EndGame;
     }
 
     // 점수를 추가하고 UI 갱신
-    public void AddScore(int newScore) {
+    public void AddScore(int newScore)
+    {
         // 게임 오버가 아닌 상태에서만 점수 증가 가능
         if (!isGameover)
         {
             // 점수 추가
             score += newScore;
+
             // 점수 UI 텍스트 갱신
             UIManager.instance.UpdateScoreText(score);
         }
     }
 
     // 게임 오버 처리
-    public void EndGame() {
+    public void EndGame()
+    {
         // 게임 오버 상태를 참으로 변경
         isGameover = true;
+
         // 게임 오버 UI를 활성화
         UIManager.instance.SetActiveGameoverUI(true);
     }
